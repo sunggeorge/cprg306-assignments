1deacb1fbc76fbe507f78e92a64142d95424a5fa
sunggeorge <sunggeorge@gmail.com>
2024-05-24 12:58:52 -0600

    week 3 assignment

--- app/page.js
+++ app/page.js
@@ -6,7 +6,8 @@ export default function Home() {
       <main>
 
           <h1 class="text-4xl font-bold mb-5">CPRG 306: Web Development 2 - Assignments</h1>
-        <h2><Link className="underline text-white-600 hover:text-cyan-300" href="./week-2/">Week 2 Assignment</Link></h2>
+        <h2><Link className="weekLinkClass" href="./week-2/">Week 2 Assignment</Link></h2>
+        <h2><Link className="weekLinkClass" href="./week-3/">Week 3 Assignment</Link></h2>
       </main>
     );
   }
